12d759181a984dbbaeb230cd07967736d49eb5c7
GaborPap <ppgabor@gmail.com>
2019-08-14 11:59:19 +0200

    for loop switched to stream

--- src/com/codecool/filepart/FileWordAnalyzer.java
+++ src/com/codecool/filepart/FileWordAnalyzer.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class FileWordAnalyzer {
     private FilePartReader filePartReader;
@@ -13,53 +14,42 @@ public class FileWordAnalyzer {
         this.filePartReader = filePartReader;
     }
 
-    public List getWordOrderedAlpabetically(){
+    public List getWordOrderedAlpabetically() {
         try {
             String str = filePartReader.read();
             List<String> items = Arrays.asList(str.split("\\r?\\n"));
             Collections.sort(items);
             return items;
 
-        } catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e);
         }
         return null;
     }
 
-    public List getWordsContainingSubstring(String subString){
+    public List getWordsContainingSubstring(String subString) {
 
         try {
             String str = filePartReader.read();
             List<String> items = Arrays.asList(str.split("\\r?\\n"));
-            List<String> contiainItems = new ArrayList<>();
-            for (String string : items){
-                if (string.contains(subString))
-                    contiainItems.add(string);
-            }
-            return contiainItems;
+            return items.stream().filter(s -> s.contains(subString)).collect(Collectors.toList());
 
 
-        } catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e);
         }
         return null;
     }
 
 
-
-    public List getStringWhichPalindromes(){
+    public List getStringWhichPalindromes() {
         try {
             String str = filePartReader.read();
             List<String> items = Arrays.asList(str.split("\\r?\\n"));
-            List<String> contiainItems = new ArrayList<>();
-            for (String string : items){
-                if (string.equals(new StringBuilder(string).reverse().toString()))
-                    contiainItems.add(string);
-            }
-            return contiainItems;
+            return items.stream().filter(s -> new StringBuilder(s).reverse().toString().equals(s)).collect(Collectors.toList());
 
 
-        } catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e);
         }
         return null;
